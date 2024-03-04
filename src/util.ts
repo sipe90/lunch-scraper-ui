ea1606ea2dad827b4adf164a97d5d586eabf9f1a
Pasi Härme <pasi.harme@gmail.com>
2024-03-04 18:37:29 +0200

    fix: fixes dates and clamp function

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,9 @@
 import { addDays, format, getWeek, startOfWeek } from 'date-fns'
 import { Menu, MenuItem } from './menu-service.js'
+
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
+import * as R from 'remeda'
 
 const WEEKDAYS = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai']
 
@@ -11,14 +13,17 @@ export const dirname = (filename: string) => path.dirname(filename)
 
 export const generateTemplateVars = (menus: Menu[]) => {
     const currentWeek = getWeek(new Date(), { weekStartsOn: 1 })
-    const weekDates = WEEKDAYS.map((day, i) => `${day} ${format(addDays(startOfWeek(new Date()), i), "d.M")}`)
+    const weekDates = WEEKDAYS.map((day, i) => `${day} ${format(addDays(startOfWeek(new Date(), { weekStartsOn: 1 }), i), "d.M")}`)
 
     return { menus, weekDates, currentWeek }
 }
 
-export const clampWeekMenu = (weekMenu: MenuItem[][]) => {
+export const clampWeekMenu = (weekMenu?: MenuItem[][]) => {
+    if (!weekMenu) {
+        return R.range(0, 5).map(() => [])
+    }
     if (weekMenu.length < 5) {
-        return weekMenu.concat(...Array(5 - weekMenu.length).map(() => []))
+        return weekMenu.concat(...R.range(0, 5 - weekMenu.length).map(() => []))
     }
     if (weekMenu.length > 5) {
         return weekMenu.slice(0, 4)
